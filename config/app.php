e6c3ad34c87eb597374c992b1d143ad8a83771ef
George Daou <georgehdaou@yahoo.com>
2016-10-18 07:58:38 -0400

    Modified Composer.json

--- config/app.php
+++ config/app.php
@@ -177,8 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Magyarjeti\LaravelLipsum\LipsumServiceProvider,
+        Magyarjeti\LaravelLipsum\LipsumServiceProvider::class,
 
     ],
 
@@ -228,7 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Lipsum' => Magyarjeti\LaravelLipsum\LipsumFacade,
+        'Lipsum' => Magyarjeti\LaravelLipsum\LipsumFacade::class,
 
     ],
 
